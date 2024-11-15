45083d4565c7de0981644e79ee2fabac78e48319
Calli Herrmann <calliherrmann@gmail.com>
2024-11-15 12:09:58 -0500

    Add mock cake and category data

---
+++ src/services/mockData.ts
@@ -0,0 +1,11 @@
+export const mockCakes = [
+  { id: 1, name: 'Chocolate Cake', category_id: 1, image_url: 'https://res.cloudinary.com/dfgkwqmtp/image/upload/v1731612277/2E17657B-7394-4790-950E-6F14D0A0996F_vm7ug8.jpg' },
+  { id: 2, name: 'Smith Cake', category_id: 2, image_url: 'https://res.cloudinary.com/dfgkwqmtp/image/upload/v1731610789/IMG_5701_oltsiu.jpg' },
+  { id: 3, name: 'Joe & Bea Cake', category_id: 3, image_url: 'https://res.cloudinary.com/dfgkwqmtp/image/upload/v1731611928/27030CB5-C659-4BF4-9A49-6ABFF019549C_xogspj.jpg' },
+];
+
+export const mockCategories = [
+  { id: 1, category: 'Birthday' },
+  { id: 2, category: 'Wedding' },
+  { id: 3, category: 'Anniversary' }
+];
